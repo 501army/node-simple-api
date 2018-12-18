c20a863d84cd35d9ee81d67b292da04dc448f7bf
Gavinda Kinandana <gavindakinandana@gmail.com>
2018-12-18 16:09:33 +0700

    add application

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Simple API using NodeJs
 
 How to use ?
 1. clone it
-2. npm install
-3. npm start
+2. rename sample.env.js to env.js and setting your environment
+3. npm install
+4. npm start
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const env = require('./env')
+const PORT = env.port
+const HOST = env.host
+const app = express()
+const bodyParser = require('body-parser')
+const faker = require('faker')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello there, welcome')
+})
+
+app.get('/user', (req, res) => {
+    let users = []
+    for (i = 0; i <= 5; i++) {
+        users.push({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email()
+        });
+    }
+    res.status(200).send({status:'200',message:'success',data:users});
+})
+
+app.listen(PORT, HOST)
+console.log(`App running on http://${HOST}:${PORT}`)
